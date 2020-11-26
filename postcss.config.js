7f5816c694453d37dca573f1a374e2da41485a63
Arcath <adam@arcath.net>
2020-11-26 20:17:47 +0000

    fix purgecss with tailwind and typography

--- postcss.config.js
+++ postcss.config.js
@@ -4,16 +4,14 @@ const purgecss = [
     // Specify the paths to all of the template files
     content: [
       './pages/**/*.{js,jsx,ts,tsx}',
-      './components/**/*.{js,jsx,ts,tsx}',
+      './lib/**/*.{js,jsx,ts,tsx}',
+      './styles/**/*.css'
     ],
-    // This is the function used to extract class names from the templates
     defaultExtractor: (content) => {
-      // Capture as liberally as possible, including things like `h-(screen-1.5)`
-      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
-      // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-      const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
-      return broadMatches.concat(innerMatches);
-    },
+      const matches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+      return matches
+    }
   },
 ];
 
