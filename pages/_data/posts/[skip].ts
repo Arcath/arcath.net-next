ce7df4364858f13c79038fd28eca6dd5d53a683a
Arcath <adam@arcath.net>
2020-11-26 22:00:06 +0000

    load posts in archive from function

--- pages/_data/posts/[skip].ts
+++ pages/_data/posts/[skip].ts
@@ -6,10 +6,10 @@ import data from './data.json'
 export async function getServerSideProps({res, params}: GetServerSidePropsContext<{skip: string}>){
   const {skip} = params
 
-  const posts = data.slice(parseInt(skip), parseInt(skip) + 1)
+  const posts = data.slice(parseInt(skip), parseInt(skip) + 5)
 
   res.end(JSON.stringify(posts))
-  return {}
+  return {props: {}}
 }
 
 export default function NullPage() {
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,5 +1,8 @@
+import React, {useState} from 'react'
 import {GetStaticPropsContext, NextPage, InferGetStaticPropsType} from 'next'
 import Link from 'next/link'
+import useSWR from 'swr'
+import {ArrayElement} from '@arcath/utils'
 
 import {getPosts, Post} from '../lib/data/posts'
 
@@ -32,25 +35,49 @@ export const getStaticProps = async ({}: GetStaticPropsContext) => {
   }
 }
 
-const PostsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({posts}) => (
-  <Layout>
+const fetcher = (resource, init) => fetch(resource, init).then(res => res.json())
+
+const PostsBlock: React.FC<{i: number}> = ({i}) => {
+  const {data: posts} = useSWR(`/_data/posts/${i * 5}.json`, fetcher)
+
+  if(!posts){
+    return <>Loading...</>
+  }
+
+  return <>
+    {posts.map(({title, href, day, month, year, lead}) => {
+      return [
+        <div key={`${href}-meta`} className="col-start-2 grid grid-cols-2 gap-2">
+          <div className="text-3xl text-center col-span-2">{day}</div>
+          <div className="text-right">{MONTH_FROM_STRING[month]}</div>
+          <div className="">{year}</div>
+        </div>,
+        <div key={`${href}-data`} className="col-start-3">
+          <Link href={href}><h3 style={{marginTop: '0'}}>{title}</h3></Link>
+          <p>{lead}</p>
+        </div>
+      ]
+    })}
+  </>
+}
+
+const PostsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = () => {
+  const [page, setPage] = useState(0)
+  
+  const pages = []
+  for(let i = 0; i <= page; i++){
+    pages.push(<PostsBlock key={i} i={i} />)
+  }
+
+  return <Layout>
     <div className="grid grid-cols-content prose dark:prose-dark max-w-none">
       <h2 className="col-start-3">Recent Posts</h2>
-      {posts.map(({title, href, day, month, year, lead}) => {
-        return [
-          <div key={`${href}-meta`} className="col-start-2 grid grid-cols-2 gap-2">
-            <div className="text-3xl text-center col-span-2">{day}</div>
-            <div className="text-right">{MONTH_FROM_STRING[month]}</div>
-            <div className="">{year}</div>
-          </div>,
-          <div key={`${href}-data`} className="col-start-3">
-            <Link href={href}><h3 style={{marginTop: '0'}}>{title}</h3></Link>
-            <p>{lead}</p>
-          </div>
-        ]
-      })}
+      {pages}
+      <button onClick={() => {
+        setPage(page + 1)
+      }}>Load More</button>
     </div>
   </Layout>
-)
+}
 
 export default PostsPage
