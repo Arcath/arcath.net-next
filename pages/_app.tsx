9cfbc78d362b4500d8cee4e5948344f4eceba00f
Adam Laycock <adam@arcath.net>
2021-02-18 20:08:32 +0000

    fix font not being included

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {AppProps} from 'next/app'
 
 import '../styles/index.css'
+import 'fontsource-montserrat'
 
 const App: React.FC<AppProps> = ({Component, pageProps}) => {
   return <Component {...pageProps} />
