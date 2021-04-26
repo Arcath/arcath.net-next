3e0e8a05c25c686098f0b3cd814cdbbc36f418b4
Adam Laycock <adam@arcath.net>
2021-04-26 13:24:12 +0100

    use colocate-images to copy images to the correct path

--- lib/functions/prepare-mdx.ts
+++ lib/functions/prepare-mdx.ts
@@ -1,19 +1,32 @@
 import path from 'path'
 import {bundleMDX} from 'mdx-bundler'
 import remarkHighlight from 'remark-highlight.js'
+import {colocateImagesPlugin} from 'remark-plugin-colocate-images'
 
-export const prepareMDX = async (source: string, files?: Record<string, string>) => { 
+export const prepareMDX = async (source: string, options: {
+  files?: Record<string, string>,
+  directory?: string
+  imagesUrl?: string
+}) => { 
   if(process.platform === "win32"){
     process.env.ESBUILD_BINARY_PATH = path.join(process.cwd(), 'node_modules', 'esbuild', 'esbuild.exe')
   }else{
     process.env.ESBUILD_BINARY_PATH = path.join(process.cwd(), 'node_modules', 'esbuild', 'bin', 'esbuild')
   }
 
+  const {files, directory, imagesUrl} = options
+
   const {code} = await bundleMDX(source, {
     files,
     xdmOptions: (input, options) => {
       options.remarkPlugins = [
-        remarkHighlight
+        ...(options.remarkPlugins ?? []),
+        remarkHighlight,
+        colocateImagesPlugin({
+          diskRoot: directory,
+          urlReplace: imagesUrl ? imagesUrl : '/img/',
+          diskReplace: imagesUrl ? path.join(process.cwd(), 'public', imagesUrl) : path.join(process.cwd(), 'public', 'img')
+        })
       ]
       
       return options
