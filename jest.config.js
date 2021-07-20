33a729eb3de7c0f006bb45f156154f97f9cd0a4f
Adam Laycock <adam@arcath.net>
2021-07-20 17:41:40 +0100

    use jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,14 @@
 module.exports = {
-  "roots": [
-    "<rootDir>"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest"
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest'
   },
-  "testRegex": "(/tests/.*|(\\.|/)(test|spec))\\.(t|j)sx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-  "collectCoverage": true,
-  "moduleNameMapper": {
-    "~/lib/(.*)": ["<rootDir>/lib/$1"],
-    "~/data/(.*)": ["<rootDir>/_data/$1"]
+  testEnvironment: 'jsdom',
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(t|j)sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  moduleNameMapper: {
+    '~/lib/(.*)': ['<rootDir>/lib/$1'],
+    '~/data/(.*)': ['<rootDir>/_data/$1']
   }
 }
