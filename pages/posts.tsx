cdf9731b4b7790105931796b931722857c26f595
Arcath <adam@arcath.net>
2021-07-26 20:26:43 +0100

    remove loader and just display all the posts

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -23,8 +23,15 @@ export const POST_FIELDS: (keyof (PostFrontmatter & PostProperties))[] = [
   'lead'
 ]
 
+export type PostData = {
+  title: string
+  href: string
+  date: string
+  lead: string
+}
+
 export const getStaticProps = async ({}: GetStaticPropsContext) => {
-  const postFiles = await getPosts()
+  const postFiles = await getPosts({limit: false})
 
   const posts: PostData[] = await asyncMap(postFiles, async post => {
     return replaceProperty(pick(await post.data, POST_FIELDS), 'date', date =>
@@ -39,92 +46,9 @@ export const getStaticProps = async ({}: GetStaticPropsContext) => {
   }
 }
 
-export type PostData = {
-  title: string
-  href: string
-  date: string
-  lead: string
-}
-
-const fetcher = (resource, init) =>
-  fetch(resource, init).then(res => res.json())
-
-const PostsBlock: React.FC<{
-  i: number
-  initialData?: PostData[]
-}> = ({i, initialData}) => {
-  let posts: PostData[]
-
-  if (initialData) {
-    posts = initialData
-  } else {
-    const {data} = useSWR(`/_data/posts/${i * 5}.json`, fetcher)
-
-    posts = data
-
-    if (!posts) {
-      return <>Loading...</>
-    }
-  }
-
-  return (
-    <>
-      {posts.map(({title, href, date, lead}) => {
-        return [
-          <div key={`${href}-meta`} className="col-start-2">
-            <PostDate date={date} />
-          </div>,
-          <div key={`${href}-data`} className="col-start-3">
-            <h3 style={{marginTop: '0'}}>
-              <Link href={href}>
-                <a>{title}</a>
-              </Link>
-            </h3>
-            <p>{lead}</p>
-          </div>
-        ]
-      })}
-    </>
-  )
-}
-
 const PostsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   posts
 }) => {
-  const [page, setPage] = useState(1)
-  const loader = useRef(null)
-
-  const pages = []
-  for (let i = 0; i <= page; i++) {
-    pages.push(
-      <PostsBlock key={i} i={i} initialData={i === 0 ? posts : undefined} />
-    )
-  }
-
-  const handleObserver: IntersectionObserverCallback = entities => {
-    const target = entities[0]
-    if (target.isIntersecting) {
-      setPage(page => page + 1)
-    }
-  }
-
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '20px',
-      threshold: 1.0
-    }
-
-    const observer = new IntersectionObserver(handleObserver, options)
-    if (loader.current) {
-      observer.observe(loader.current)
-    }
-
-    return () => {
-      observer.disconnect()
-    }
-  }, [])
-
   return (
     <Layout>
       <Head>
@@ -136,17 +60,21 @@ const PostsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
       />
       <div className="grid grid-cols-content prose dark:prose-dark max-w-none">
         <h2 className="col-start-3">Recent Posts</h2>
-        {pages}
-        <div className="col-start-3 text-center">
-          <button
-            ref={loader}
-            onClick={() => {
-              setPage(page + 1)
-            }}
-          >
-            Load More
-          </button>
-        </div>
+        {posts.map(({title, href, date, lead}) => {
+          return [
+            <div key={`${href}-meta`} className="col-start-2">
+              <PostDate date={date} />
+            </div>,
+            <div key={`${href}-data`} className="col-start-3">
+              <h3 style={{marginTop: '0'}}>
+                <Link href={href}>
+                  <a>{title}</a>
+                </Link>
+              </h3>
+              <p>{lead}</p>
+            </div>
+          ]
+        })}
       </div>
     </Layout>
   )
