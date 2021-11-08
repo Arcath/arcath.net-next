fd32cf5553cb906204744ae3dcece4bf6b86e8e4
Adam Laycock <adam@arcath.net>
2021-11-08 13:13:50 +0000

    swap to using octokit for getting posts.

--- pages/api/posts/all.ts
+++ pages/api/posts/all.ts
@@ -1,9 +1,58 @@
 import {NextApiHandler} from 'next'
-
-import {getPosts} from '../../../lib/data/posts'
+import {Octokit} from 'octokit'
+import {asyncMap} from '@arcath/utils/lib/functions/async-map'
+import matter from 'gray-matter'
 
 const allPostsHandler: NextApiHandler = async (req, res) => {
-  const posts = await getPosts({limit: false})
+  const octokit = new Octokit({
+    auth: process.env.GH_TOKEN
+  })
+
+  const main = await octokit.request(
+    'GET /repos/{owner}/{repo}/branches/{branch}',
+    {owner: 'arcath', repo: 'arcath.net-next', branch: 'main'}
+  )
+
+  const tree_sha = main.data.commit.sha
+
+  const tree = await octokit.request(
+    'GET /repos/{owner}/{repo}/git/trees/{tree_sha}',
+    {
+      owner: 'arcath',
+      repo: 'arcath.net-next',
+      tree_sha,
+      recursive: 'true'
+    }
+  )
+
+  const files = tree.data.tree
+
+  const postFiles = files.reduce((posts, file) => {
+    if (file.path.match('_content/posts/.*/index.mdx')) {
+      posts.push(file)
+    }
+
+    return posts
+  }, [])
+
+  const posts = await asyncMap(postFiles, async postFile => {
+    const postContent = await octokit.request(
+      'GET /repos/{owner}/{repo}/git/blobs/{file_sha}',
+      {
+        owner: 'arcath',
+        repo: 'arcath.net-next',
+        file_sha: postFile.sha
+      }
+    )
+
+    const content = Buffer.from(postContent.data.content, 'base64').toString(
+      'utf8'
+    )
+
+    const {data} = matter(content)
+
+    return data
+  })
 
   res.status(200).json(posts)
 }
